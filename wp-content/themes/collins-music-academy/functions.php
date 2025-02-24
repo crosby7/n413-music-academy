a22c27c68096d20b80229a6a4ffab2b329860681
Cameron <camcrosby09@gmail.com>
2025-02-23 19:03:45 -0500

    Content complete for all but lessons, needs majority of styles

--- wp-content/themes/collins-music-academy/functions.php
+++ wp-content/themes/collins-music-academy/functions.php
@@ -1,2 +1,16 @@
 <?php
 
+function bma_custom_blocks_init()
+{
+    register_block_type(__DIR__ . "/build/main-nav");
+}
+
+add_action("init", "bma_custom_blocks_init");
+
+
+function my_theme_assets()
+{
+    wp_enqueue_style("style", get_stylesheet_uri());
+}
+
+add_action("wp_enqueue_scripts", "my_theme_assets");
